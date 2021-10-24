7281876ed1c1c6c76949cd2e67303c235a6cd87f
Jeffry Babb <ahuimanu@gmail.com>
2021-10-24 16:20:13 -0500

    updated template expressions in posts list

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+
+from .models import Post
 
 # Create your views here.
 def post_list(request):
     # return HttpResponse("HI, this is from the Django Girls Tutorial")
-    return render(request, 'blog/post_list.html', {})    
+
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+    return render(request, 'blog/post_list.html', {'posts' : posts})
