a3de6819f67d8f95763342806edecdc9867cadcf
Jeffry Babb <ahuimanu@gmail.com>
2021-10-24 15:33:10 -0500

    templates from the Django Girls Tutorial

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def post_list(request):
+    # return HttpResponse("HI, this is from the Django Girls Tutorial")
+    return render(request, 'blog/post_list.html', {})    
